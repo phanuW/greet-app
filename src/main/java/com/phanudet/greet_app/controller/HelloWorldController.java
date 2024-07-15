392c39eaffb96c51ce49268e74c28aaad062ddc1
phanudet@hpt <phanudet.w@hyper-trade.com>
2024-07-15 14:06:28 +0700

    Changed message greet

--- src/main/java/com/phanudet/greet_app/controller/HelloWorldController.java
+++ src/main/java/com/phanudet/greet_app/controller/HelloWorldController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
     @GetMapping("/hello")
     public String sayHello() {
-        return "Hello World";
+        return "Hello World - 1234";
     }
 }
